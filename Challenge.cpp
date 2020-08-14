42d7957808130f4d7d473f98e14b14382cee4378
infoprogrammr <68546554+infoprogrammr@users.noreply.github.com>
2020-08-14 17:31:27 +0530

    Create Challenge.cpp

---
+++ Challenge.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string input,rev="";
+    
+    cout << "Enter the string:";
+    cin >> input;
+
+    // uncomment below line 
+    // rev = string(input.rbegin(),input.rend());
+
+    // Prints reverse of the string 
+    cout << rev;     
+
+    return 0;
+}
